e8cc802c9935b424391d081d624a328a2494c3bc
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-05-27 21:17:42 +0530

    Uses of Yield Keyword in c#

---
+++ Source/C#/Uses of Yield Keyword/Uses of Yield Keyword/MergeTwoLists.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uses_of_Yield_Keyword
+{
+    public class MergeTwoLists
+    {
+        List<string> MarutiCars = new List<string>
+                                           {
+                                               "WagonR", "Celerio", "Alto K10"
+                                           };
+        List<string> HyundaiCars = new List<string>
+                                           {
+                                               "EON", "i10", "i20", "Verna"
+                                           };
+        public void Merge()
+        {
+
+            var allModels = AllCarModelsEfficientMerge(MarutiCars, HyundaiCars);
+
+            foreach (var model in allModels)
+            {
+                Console.WriteLine(model);
+            }
+        }
+
+
+        static IEnumerable<string> AllCarModelsEfficientMerge(List<string> marutiCars, List<string> hyundaiCars)
+        {
+            foreach (var m in marutiCars)
+                yield return m;
+
+            foreach (var h in hyundaiCars)
+                yield return h;
+        }
+
+    }
+}
---
+++ Source/C#/Uses of Yield Keyword/Uses of Yield Keyword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Uses_of_Yield_Keyword
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> randomIntegerList = new List<int>
+                                           {
+                                               23,84,99,67,92,14,78,43,28
+                                           };
+
+            var evenNumberList = GetEvenNumber(randomIntegerList);
+
+            foreach (var number in evenNumberList)
+            {
+                Console.WriteLine(number);
+            }
+
+            MergeTwoLists mergeTwoList = new MergeTwoLists();
+            mergeTwoList.Merge();
+        }
+
+        public static IEnumerable<int> GetEvenNumber(List<int> randomIntegerList)
+        {
+            foreach (var number in randomIntegerList)
+            {
+                if (number % 2 == 0)
+                    yield return number;
+            }
+        }
+    }
+}
