c0ca4172aaad4f0d707a894e74946a63c08ef4c9
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-05-27 15:17:42 +0530

    ExAMPLE - difference-between-class-and-structure

---
+++ Source/C#/PlayingWithStructureAndClass/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace PlayingWithStructureAndClass
+{
+    struct Employee
+    {
+        public int EmpID { get; private set; }
+
+        public Employee(int id)
+        {
+            EmpID = id;
+        }
+
+        public int IncrementEmpID()
+        {
+            EmpID++;
+            return EmpID;
+        }
+    }
+
+    class Program
+    {
+        public Employee PropEmployee { get; } = new Employee(10);
+        public readonly Employee ReadonlyEmployee = new Employee(10);
+        public Employee FieldMemberEmployee = new Employee(10);
+
+        public void Test()
+        {
+            Console.WriteLine("===========Property case=============");
+            Console.WriteLine(PropEmployee.IncrementEmpID());
+            Console.WriteLine(PropEmployee.IncrementEmpID());
+
+            Console.WriteLine("===========Readonly case=============");
+            Console.WriteLine(ReadonlyEmployee.IncrementEmpID());
+            Console.WriteLine(ReadonlyEmployee.IncrementEmpID());
+
+            Console.WriteLine("===========Field Member case==========");
+            Console.WriteLine(FieldMemberEmployee.IncrementEmpID());
+            Console.WriteLine(FieldMemberEmployee.IncrementEmpID());
+        }
+
+        static void Main(string[] args)
+        {
+
+            new Program().Test();
+        }
+    }
+}
+
